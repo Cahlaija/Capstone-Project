989280764bd1f9f1bfcac8eec88afa1f4e1e34ad
Cahlaija <Cahlaijak@yahoo.com>
2026-08-03 11:12:30 -0400

    Create product routes

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,14 @@ dotenv.config();
 
 connectDB();
 
+const productRoutes = require("./routes/productRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Inventory API is running...");
