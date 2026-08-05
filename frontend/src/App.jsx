30bcaa95c166a8b444b3f1bf43db40f515a40230
Cahlaija <Cahlaijak@yahoo.com>
2026-08-05 09:36:53 -0400

    Improve inventory manager UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,74 +1,9 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+return (
+  <div>
+    <Navbar />
 
-import Navbar from "./components/Navbar";
-
-import Home from "./pages/Home";
-import AddProduct from "./pages/AddProduct";
-import ProductList from "./pages/ProductList";
-import EditProduct from "./pages/EditProduct";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import NotFound from "./pages/NotFound";
-
-function App() {
-  const [refresh, setRefresh] = useState(false);
-
-  const handleProductAdded = () => {
-    setRefresh(!refresh);
-  };
-
-  return (
-    <div>
-      <Navbar />
-
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-
-        <Route
-          path="/products"
-          element={
-            <ProductList
-              refresh={refresh}
-            />
-          }
-        />
-
-        <Route
-          path="/add"
-          element={
-            <AddProduct
-              onProductAdded={handleProductAdded}
-            />
-          }
-        />
-
-        <Route
-          path="/edit/:id"
-          element={<EditProduct />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-
-        {/* Catch-all 404 route */}
-        <Route
-          path="*"
-          element={<NotFound />}
-        />
-      </Routes>
-    </div>
-  );
-}
-
-export default App;
+    <Routes>
+      ...
+    </Routes>
+  </div>
+);
