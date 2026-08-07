f2c336338fdb95ec8eefa08f2153dab6fbb73f90
Cahlaija <Cahlaijak@yahoo.com>
2026-08-07 12:26:57 -0400

    Prepare backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ const app = express();
 
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
 
 
@@ -31,8 +36,10 @@ mongoose
   .then(() => {
     console.log("✅ MongoDB Connected");
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((error) => {
