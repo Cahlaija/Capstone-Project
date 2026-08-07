ec60155aa2f226ab48161734f630d9aea5094107
Cahlaija <Cahlaijak@yahoo.com>
2026-08-07 11:36:30 -0400

    Add Bootstrap styling framework

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,73 @@
-return (
-  <div>
-    <Navbar />
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-    <Routes>
-      ...
-    </Routes>
-  </div>
-);
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
+import AddProduct from "./pages/AddProduct";
+import EditProduct from "./pages/EditProduct";
+import NotFound from "./pages/NotFound";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+function App() {
+  const [refresh, setRefresh] = useState(false);
+
+  const handleProductAdded = () => {
+    setRefresh((prev) => !prev);
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: "1200px",
+        margin: "0 auto",
+        padding: "20px",
+      }}
+    >
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/products"
+          element={
+            <ProtectedRoute>
+              <ProductList refresh={refresh} />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/add"
+          element={
+            <ProtectedRoute>
+              <AddProduct onProductAdded={handleProductAdded} />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/edit/:id"
+          element={
+            <ProtectedRoute>
+              <EditProduct />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
